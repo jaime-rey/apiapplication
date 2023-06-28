d920f7cae58ba7e4c969ecb4c1afa84366fa4b73
jaime-rey <jaimereycasado@gmail.com>
2023-06-28 12:41:49 +0200

    secon commit 2

--- src/main/java/io/getarrays/apiapplication/repository/JpaEmployeeRepository.java
+++ src/main/java/io/getarrays/apiapplication/repository/JpaEmployeeRepository.java
@@ -1,7 +1,11 @@
 package io.getarrays.apiapplication.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import io.getarrays.apiapplication.model.Employee;
+
+@Repository
 public interface JpaEmployeeRepository extends JpaRepository<Employee, Integer> {
-
+    // Define more if need be added
 }
